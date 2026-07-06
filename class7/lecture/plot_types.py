99b702904204d9a6b25ce7aef2f61a6d52e0db86
Pedro Paredes <759984+gangsterveggies@users.noreply.github.com>
2026-07-06 02:30:18 -0400

    Replace plt.show() with plt.savefig("plot.png") in lecture files for class 7

--- class7/lecture/plot_types.py
+++ class7/lecture/plot_types.py
@@ -6,16 +6,16 @@ y = [2, 4, 1, 3, 5]
 # Line plot -- trends and change over time
 plt.plot(x, y)
 plt.title("Line Plot")
-plt.show()
+plt.savefig("plot.png")
 
 # Scatter plot -- one dot per data point
 plt.scatter(x, y)
 plt.title("Scatter Plot")
-plt.show()
+plt.savefig("plot.png")
 
 # Bar chart -- comparing amounts across categories
 labels = ["A", "B", "C", "D", "E"]
 values = [3, 7, 2, 5, 4]
 plt.bar(labels, values)
 plt.title("Bar Chart")
-plt.show()
+plt.savefig("plot.png")
